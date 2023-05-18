218dbb32d573cf10da000c7c84eb3c9d43d07ae1
heider marquez <heidermadiaz@gmail.com>
2023-05-18 11:05:58 -0500

    1ra vez imprimiendo la hora en front

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,17 +6,31 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('choose a location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: Text('Choose location screen'),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('choose a location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+          child: Text('counter is $counter'),
+        ));
   }
 }
