5e96608e07a8ad159c32954a6c1f686f9a0f5776
heider marquez <heidermadiaz@gmail.com>
2023-05-21 14:04:13 -0500

    agregamos varias ubicaciones

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/routes.dart';
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,20 +11,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Map<String, dynamic> data = {};
   @override
   Widget build(BuildContext context) {
+    data =
+        ModalRoute.of(context)?.settings?.arguments as Map<String, dynamic>? ??
+            {};
+    print(data);
+
+    // print(data2);
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            FloatingActionButton.extended(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              FloatingActionButton.extended(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location'),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: TextStyle(fontSize: 28.0, letterSpacing: 2.8),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  fontSize: 66.0,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 import 'dart:convert';
 
 class worldTime {
@@ -6,32 +7,36 @@ class worldTime {
   String time = ''; // the time in that locaction
   String flag = ''; //url to an asset flag icon
   String url = ''; //locacion url for api endppoint
+  bool isDaytime = false;
 
   worldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    //make the reques
-    var url1 = Uri.parse('https://worldtimeapi.org/api/timezone/$url');
-    Response response = await get(url1);
+    try {
+      //make the reques
+      var url1 = Uri.parse('https://worldtimeapi.org/api/timezone/$url');
+      Response response = await get(url1);
 
-    Map data = jsonDecode(response.body);
-    //print(data);
-    //get propertis from data
+      Map data = jsonDecode(response.body);
+      //print(data);
+      //get propertis from data
 
-    String datatime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    //print(datatime);
-    //print(offset);
+      String datatime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      //print(datatime);
+      //print(offset);
 
-    //create DateTime object
+      //create DateTime object
 
-    DateTime now = DateTime.parse(datatime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      DateTime now = DateTime.parse(datatime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
 //set time property
-    time = now.toString();
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
+    } catch (e) {
+      print('caught error: $e');
+      time = 'could not get time date';
+    }
   }
 }
-
-worldTime instance =
-    worldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
